d2b4694e55e4a1156e5352c62c6ceff17b6e4bf2
I.Safonov <safonovil17@yandex.ru>
2024-01-19 14:17:49 +0300

    create functions clearPage and showQuestion

---
+++ js/functions.js
@@ -0,0 +1,26 @@
+function clearPage() {
+	questionContainer.innerHTML = ''
+	answersContainer.innerHTML = ''
+}
+
+function showQuestion() {
+	// Вывод вопроса
+	const question = questions[questionIndex]['question']
+	const questionTemplate = `
+		<h2 class="title">${question}</h2>
+	`
+	questionContainer.innerHTML = questionTemplate
+
+	// Вывод ответов
+	for (answer of questions[questionIndex]['answers']) {
+		const answerTemplate = `
+			<li>
+				<label>
+					<input type="radio" class="answer" name="answer" />
+					<span>${answer}</span>
+				</label>
+			</li>
+		`
+		answersContainer.insertAdjacentHTML('afterbegin', answerTemplate)
+	}
+}
